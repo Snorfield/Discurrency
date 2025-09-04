a3a66375d8ddfb3e6d691c2cd50e864de437696a
Garfield <overweightcatgarfield@gmail.com>
2025-09-04 12:44:05 -0600

    Added user shops

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ const { token } = require('./config.json');
 
 // Database Setup
 
-const db = require('better-sqlite3')('economy.db');
+const economy = require('better-sqlite3')('economy.db');
 
-db.prepare(`
+// Stores user balances
+economy.prepare(`
   CREATE TABLE IF NOT EXISTS users (
     user_id TEXT PRIMARY KEY,
     balance INTEGER DEFAULT 100
+  )
+`).run();
+
+// Stores user shop data (not implemented yet)
+economy.prepare(`
+  CREATE TABLE IF NOT EXISTS shops (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      user_id TEXT NOT NULL,
+      price INTEGER DEFAULT 0,
+      service TEXT DEFAULT '',
+      description TEXT DEFAULT ''
   )
 `).run();
 
@@ -39,9 +51,9 @@ client.on(Events.InteractionCreate, async interaction => {
 
     let userId = interaction.user.id;
 
-    db.prepare('INSERT OR IGNORE INTO users (user_id, balance) VALUES (?, 100)').run(userId);
+    economy.prepare('INSERT OR IGNORE INTO users (user_id, balance) VALUES (?, 100)').run(userId);
 
-    let row = db.prepare('SELECT balance FROM users WHERE user_id = ?').get(userId);
+    let row = economy.prepare('SELECT balance FROM users WHERE user_id = ?').get(userId);
 
     let balance = row.balance;
 
@@ -52,9 +64,9 @@ client.on(Events.InteractionCreate, async interaction => {
     let userId = interaction.user.id;
     let amount = Math.abs(interaction.options.getNumber('amount'));
 
-    db.prepare('INSERT OR IGNORE INTO users (user_id, balance) VALUES (?, 100)').run(userId);
+    economy.prepare('INSERT OR IGNORE INTO users (user_id, balance) VALUES (?, 100)').run(userId);
 
-    let balance = db.prepare('SELECT balance FROM users WHERE user_id = ?').get(userId);
+    let balance = economy.prepare('SELECT balance FROM users WHERE user_id = ?').get(userId);
 
     if ((balance.balance - amount) < 0) {
 
@@ -63,24 +75,24 @@ client.on(Events.InteractionCreate, async interaction => {
     } else {
 
       let recipient = interaction.options.getUser('user').id;
-      let recipientUser = db.prepare('SELECT * FROM users WHERE user_id = ?').get(recipient);
+      let recipientUser = economy.prepare('SELECT * FROM users WHERE user_id = ?').get(recipient);
 
       if (recipientUser) {
 
-        db.prepare('UPDATE users SET balance = balance - ? WHERE user_id = ?').run(amount, userId);
-        db.prepare('UPDATE users SET balance = balance + ? WHERE user_id = ?').run(amount, recipient);
+        economy.prepare('UPDATE users SET balance = balance - ? WHERE user_id = ?').run(amount, userId);
+        economy.prepare('UPDATE users SET balance = balance + ? WHERE user_id = ?').run(amount, recipient);
 
         await interaction.reply(`Paid user <@${recipient}> ${amount} ${value(amount)}.`);
 
       } else {
         await interaction.reply('This user doesn\'t have an account. Tell them to run /balance.');
       }
-    }  
+    }
   } else if (commandName === 'leaderboard') {
 
     let leaderboard = '';
 
-    let topUsers = db.prepare(`
+    let topUsers = economy.prepare(`
       SELECT user_id, balance
       FROM users
       ORDER BY balance DESC
@@ -88,18 +100,85 @@ client.on(Events.InteractionCreate, async interaction => {
     `).all();
 
     for (let i = 0; i < topUsers.length; i++) {
-      
+
       let mention = (await client.users.fetch(topUsers[i].user_id)).username;
 
       leaderboard += `**${mention}**: ${Math.round(Number(topUsers[i].balance))} \n`;
     }
-    
+
     let embed = new EmbedBuilder()
       .setTitle('Top Users By Tokens')
       .setDescription(leaderboard)
       .setTimestamp()
 
     await interaction.reply({ embeds: [embed] });
+  } else if (commandName === 'shop') {
+
+    let target = interaction.options.getUser('user').id;
+    let userShops = economy.prepare('SELECT * FROM shops WHERE user_id = ?').all(target);
+
+    if (userShops.length > 0) {
+
+      let text = '';
+
+      for (let i = 0; i < userShops.length; i++) {
+        let item = userShops[i];
+        text += `**#${item.id} - ${item.service}**\n*${item.price} Tokens*\n${item.description}\n\n`;
+      }
+
+      let mention = (await client.users.fetch(target)).username;
+
+      let embed = new EmbedBuilder()
+        .setTitle(`${mention}'s Shop`)
+        .setDescription(text)
+        .setTimestamp()
+
+      await interaction.reply({ embeds: [embed] });
+    } else {
+      await interaction.reply("This user doesn't have any shops!")
+    }
+
+  } else if (commandName === 'create-product') {
+
+    let userId = interaction.user.id;
+
+    let userShops = economy.prepare('SELECT * FROM shops WHERE user_id = ?').all(userId);
+
+    if (userShops.length >= 4) {
+      await interaction.reply("You have the limit of four products in your shop, please remove some to add this product.");
+    } else {
+      economy.prepare('INSERT OR IGNORE INTO users (user_id, balance) VALUES (?, 100)').run(userId);
+
+      let product = interaction.options.getString('product');
+      let description = interaction.options.getString('description');
+      let price = interaction.options.getNumber('price');
+
+      economy.prepare(`INSERT INTO shops (user_id, service, description, price) VALUES (?, ?, ?, ?);`).run(userId, product, description, price);
+
+      let text = `**${product}**\n*${price} Tokens*\n${description}`;
+
+      let embed = new EmbedBuilder()
+        .setTitle(`Product Successfully Created`)
+        .setDescription(text)
+        .setTimestamp()
+
+      await interaction.reply({ embeds: [embed] });
+    }
+  } else if (commandName === 'remove-product') {
+
+    let userId = interaction.user.id;
+
+    let productId = interaction.options.getNumber('id');
+
+    let product = economy.prepare('SELECT * FROM shops WHERE id = ? AND user_id = ?').get(productId, userId);
+
+    if (product) {
+      economy.prepare('DELETE FROM shops WHERE id = ? AND user_id = ?').run(productId, userId);
+      await interaction.reply("Product successfully removed from your shop.");
+    } else {
+      await interaction.reply("You don't own a product listing with that id!");
+    }
+
   }
 });
 
